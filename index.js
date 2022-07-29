ae736466691d241b6c8f9b55412ebf63f80c1f14
Pham Minh Hy <hypm@MECORP.LOCAL>
2022-07-29 09:38:34 +0700

    update

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+const bodyParser = require('body-parser');
+const multer  = require('multer');
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const upload = multer({ dest: 'uploads/' })
+
 var app = express();
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 app.use(cors());
+app.use(express.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), async function (req, res) {
+  console.log(req.file);
+  if (req.file) {
+    const { originalname, mimetype, size } = req.file;
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size
+    });
+  } else {
+    res.json('File not found')
+  }
+});
 
 
 const port = process.env.PORT || 3000;
